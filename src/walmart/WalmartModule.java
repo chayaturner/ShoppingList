3689a5c80881d4d2762d28044aa0439ee08c59a7
renakerstein <renakerstein>
2016-03-31 20:23:15 -0400

    Project in progress

--- src/walmart/WalmartModule.java
+++ src/walmart/WalmartModule.java
@@ -6,9 +6,10 @@ import com.google.inject.Module;
 public class WalmartModule implements Module {
 
 	@Override
-	public void configure(Binder arg0) {
-
-
+	public void configure(Binder binder) {
+    WalmartComponents components = new WalmartComponents();
+    
+    binder.bind(WalmartComponents.class).toInstance(components);
 	}
 
 }
--- src/walmart/WalmartSearchGui.java
+++ src/walmart/WalmartSearchGui.java
@@ -11,25 +11,26 @@ public class WalmartSearchGui extends JFrame {
 
 	private static final long serialVersionUID = 1L; // default
 
-	//private double totalCount;
-	//private int index;
+	// private double totalCount;
+	// private int index;
 
-@Inject
+	@Inject
 	public WalmartSearchGui(WalmartComponents components) {
 		setTitle("Walmart Search");
 		setSize(900, 700);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setLocationRelativeTo(null);
-		
+
 		setLayout(new BorderLayout());
 		add(components, BorderLayout.CENTER);
-		
-setVisible(true);
+
+		setVisible(true);
 	}
 
 	public static void main(String[] args) {
 		Injector injector = Guice.createInjector(new WalmartModule());
-		WalmartSearchGui walmartGui = injector.getInstance(WalmartSearchGui.class);
+		WalmartSearchGui walmartGui = injector
+				.getInstance(WalmartSearchGui.class);
 
 	}
 }
