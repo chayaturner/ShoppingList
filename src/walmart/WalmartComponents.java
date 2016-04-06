702d9f92f314a3cde6787d1869f1feef4c33256b
AhuvaFoxman <afoxman36@gmail.com>
2016-04-06 17:38:09 -0400

    Added to master

--- src/walmart/WalmartComponents.java
+++ src/walmart/WalmartComponents.java
@@ -22,7 +22,6 @@ import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
-import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.ListSelectionModel;
 import javax.swing.border.LineBorder;
