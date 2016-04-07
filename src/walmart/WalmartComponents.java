1259e6d803e4c10c6bbd8f181985fec043efcb0c
renakerstein <renakerstein>
2016-04-06 21:16:33 -0400

    Added cart frame, cleaned up code, changed layout of WalmartSearchGui

--- src/walmart/WalmartComponents.java
+++ src/walmart/WalmartComponents.java
@@ -13,6 +13,7 @@ import java.awt.event.KeyListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.util.ArrayList;
 
 import javax.inject.Inject;
 import javax.swing.AbstractAction;
@@ -23,27 +24,24 @@ import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
-import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.ListSelectionModel;
 import javax.swing.border.LineBorder;
 
 public class WalmartComponents extends Container {
 
-	private JLabel logoLabel, shoppingCart;
+	private JLabel  logoLabel, shoppingCart;
 	private JList<Item> resultsList;
 	private JList<Item> shoppingList;
-	private DefaultListModel<Item> shoppingListModel;
+	private ArrayList<Item> itemsInCart;
 	private JButton searchButton, addButton;
 	private JTextField searchInput;
 	private JPanel topPanel, bottomPanel, centerPanel, searchPanel,
 			productDetails, resultsPanel;
-	private LineBorder border;
 	private ImageIcon logo;
 	private Item[] items;
 	private SearchThread thread;
 	private Double totalPrice;
-	private JLabel displayTotal;
 
 	@Inject
 	public WalmartComponents() {
@@ -51,27 +49,23 @@ public class WalmartComponents extends Container {
 		Color wmBlue = new Color(65, 105, 250);
 		Color wmOrange = new Color(240, 160, 0);
 		Color lightBlue = new Color(173, 216, 230);
-		border = new LineBorder(Color.BLACK);
 
 		setLayout(new BorderLayout());
 
-		// CENTER
 		this.totalPrice = 0.0;
+		this.itemsInCart=new ArrayList<Item>();
+		 
 		centerPanel = new JPanel();
 		centerPanel.setLayout(new BoxLayout(centerPanel, BoxLayout.LINE_AXIS));
 		centerPanel.setBackground(lightBlue);
-
-		shoppingListModel = new DefaultListModel<Item>();
-		shoppingList = new JList<Item>(shoppingListModel);
-		shoppingList.setForeground(wmBlue);
-		shoppingList.setBackground(new Color(255, 231, 186));
-		shoppingList.setPreferredSize(new Dimension(200, Short.MAX_VALUE));
-		shoppingList.setMaximumSize(new Dimension(200, Short.MAX_VALUE));
-		shoppingList.setBorder(border);
+		
+		shoppingList = new JList<Item>();
 		shoppingList.setFont(font);
-		centerPanel.add(shoppingList);
+		shoppingListMouseListener();
+		
 		resultsPanel = new JPanel(new GridLayout(2, 1));
 		resultsPanel.setBackground(lightBlue);
+		
 		resultsList = new JList<Item>();
 		resultsList.setForeground(new Color(0, 0, 139));
 		resultsList.setBackground(lightBlue);
@@ -79,47 +73,107 @@ public class WalmartComponents extends Container {
 		resultsList.setLayoutOrientation(JList.VERTICAL);
 		resultsList.setVisibleRowCount(-1);
 		resultsList.setFont(font);
-		productDetails = new JPanel(new GridLayout(4, 1));
+		MouseListener mouseListener = resultsMouseListener();
+		resultsList.addMouseListener(mouseListener);
+		
+		productDetails = new JPanel(new GridLayout(2, 1));
 		productDetails.setBackground(lightBlue);
+		
 		resultsPanel.add(resultsList);
 		resultsPanel.add(productDetails);
+		
 		centerPanel.add(resultsPanel);
-
 		add(centerPanel, BorderLayout.CENTER);
 
-
-
-		topPanel = new JPanel(new FlowLayout(FlowLayout.CENTER,25,3));
+		topPanel = new JPanel(new FlowLayout(FlowLayout.CENTER,20,3));
 		topPanel.setBackground(Color.WHITE);
-		logo = new ImageIcon("logo.png");
-		logoLabel = new JLabel(logo);
-		topPanel.add(logoLabel);
 		
-		searchPanel = new JPanel(new GridLayout(2,6));
-		searchPanel.setBackground(Color.WHITE);
-		searchInput = new JTextField("",12);
+		searchInput = new JTextField("",10);
+		searchInput.setFont(font);
 		searchInput.setHorizontalAlignment(JTextField.CENTER);
 		searchInput.setBorder(new LineBorder(wmBlue));
 		searchInput.setForeground(wmBlue);
-		searchInput.setMaximumSize(new Dimension(300, 35));	
-
 		addEnterKeyListener();
+
+		logo = new ImageIcon("logo.png");
+		logoLabel = new JLabel(logo);
+		
+		searchPanel = new JPanel(new GridLayout(3, 1));
+		searchPanel.setBackground(Color.WHITE);
+		
 		searchButton = new JButton("Search");
 		searchButton.setForeground(wmOrange);
 		searchButton.setSize(50, 30);
-		searchButton.setMaximumSize(new Dimension(300, 35));
+		addSearchActionListener();
+		
 		searchPanel.add(searchInput);
 		searchPanel.add(searchButton);
+		
 		shoppingCart=new JLabel();
-		shoppingCart.setIcon(new ImageIcon("shoppingCart.png"));
-		shoppingCart.setMaximumSize(new Dimension(300, 35));
+		shoppingCart.setIcon(new ImageIcon("cart.png"));
+		shoppingCartClick();
 	
+		topPanel.add(logoLabel);
+		topPanel.add(searchPanel);
+		topPanel.add(shoppingCart);
+		add(topPanel, BorderLayout.NORTH);
+
+		bottomPanel = new JPanel(new BorderLayout());
+		bottomPanel.setBackground(lightBlue);
+		
+		addButton = new JButton("Like it? Add to shopping cart!");
+		addButton.setForeground(Color.BLUE);
+		addButton.setBackground(wmOrange);
+		addButton.setFont(new Font("Arial", Font.BOLD, 21));
+		addButton.setMaximumSize(new Dimension(300, 35));
+		addItemActionListener();
+		
+		bottomPanel.add(addButton, BorderLayout.CENTER);
+		add(bottomPanel, BorderLayout.SOUTH);
+		
+	
+	}
+
+	private void shoppingListMouseListener() {
+		MouseListener mouseListenerList = new MouseAdapter() {
+
+			public void mouseClicked(MouseEvent e) {
+				if (e.getClickCount() == 2) {
+
+					int index = shoppingList.locationToIndex(e.getPoint());
+
+					items = thread.getItems();
+					System.out.println(items[index]);
+					new ProductFrame(items[index]).setVisible(true);
+				}
+			}
+		};
+		shoppingList.addMouseListener(mouseListenerList);
+	}
+
+	private MouseListener resultsMouseListener() {
+		MouseListener mouseListener = new MouseAdapter() {
+
+			public void mouseClicked(MouseEvent e) {
+				if (e.getClickCount() == 2) {
+
+					int index = resultsList.locationToIndex(e.getPoint());
+
+					items = thread.getItems();
+					System.out.println(items[index]);
+					new ProductFrame(items[index]).setVisible(true);
+				}
+			}
+		};
+		return mouseListener;
+	}
+
+	private void shoppingCartClick() {
 		shoppingCart.addMouseListener(new MouseListener(){
 
 			@Override
 			public void mouseClicked(MouseEvent arg0) {
-				new ShoppingCartFrame().setVisible(true);
-				
+					new ShoppingCartFrame(shoppingList, itemsInCart, totalPrice).setVisible(true);
 			}
 
 			@Override
@@ -143,69 +197,9 @@ public class WalmartComponents extends Container {
 			}
 			
 		});
-		
-
-		topPanel.add(searchPanel);
-		topPanel.add(shoppingCart);
-		
-		
-	//	search = new JLabel("Search Walmart.com:");
-		//search.setForeground(wmOrange);
-	
-		add(topPanel, BorderLayout.NORTH);
-
-
-		AddSearchActionListener();
-		// SOUTH
-		bottomPanel = new JPanel(new BorderLayout());
-		bottomPanel.setBackground(lightBlue);
-		add(bottomPanel, BorderLayout.SOUTH);
-		addButton = new JButton("Like it? Add to shopping list!");
-		addButton.setForeground(Color.BLUE);
-		addButton.setBackground(wmOrange);
-		addButton.setMaximumSize(new Dimension(300, 35));
-		displayTotal = new JLabel();
-		displayTotal.setFont(font);
-		displayTotal.setForeground(Color.blue);
-		displayTotal.setText("            TOTAL : $0.0");
-		bottomPanel.add(displayTotal, BorderLayout.NORTH);
-		bottomPanel.add(addButton, BorderLayout.CENTER);
-
-		AddItemActionListener();
-
-		MouseListener mouseListener = new MouseAdapter() {
-
-			public void mouseClicked(MouseEvent e) {
-				if (e.getClickCount() == 2) {
-
-					int index = resultsList.locationToIndex(e.getPoint());
-
-					items = thread.getItems();
-					System.out.println(items[index]);
-					new ProductFrame(items[index]).setVisible(true);
-				}
-			}
-		};
-
-		resultsList.addMouseListener(mouseListener);
-
-		MouseListener mouseListenerList = new MouseAdapter() {
-
-			public void mouseClicked(MouseEvent e) {
-				if (e.getClickCount() == 2) {
-
-					int index = shoppingList.locationToIndex(e.getPoint());
-
-					items = thread.getItems();
-					System.out.println(items[index]);
-					new ProductFrame(items[index]).setVisible(true);
-				}
-			}
-		};
-		shoppingList.addMouseListener(mouseListenerList);
 	}
 
-	private void AddItemActionListener() {
+	private void addItemActionListener() {
 		addButton.addActionListener(new AbstractAction() {
 
 			private static final long serialVersionUID = 1L;
@@ -215,10 +209,9 @@ public class WalmartComponents extends Container {
 
 				// add to the shopping list
 				Item selected = resultsList.getSelectedValue();
-				shoppingListModel.addElement(selected);
+				itemsInCart.add(selected);
 				totalPrice += selected.getSalePrice();
-				displayTotal.setText("          TOTAL: $"
-						+ String.format("%.2f", totalPrice));
+					
 
 			}
 
@@ -226,7 +219,7 @@ public class WalmartComponents extends Container {
 	}
 
 	// user can click on button
-	private void AddSearchActionListener() {
+	private void addSearchActionListener() {
 		searchButton.addActionListener(new AbstractAction() {
 
 			private static final long serialVersionUID = 1L;
